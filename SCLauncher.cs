ff0aeb694aaa36dac15cbafba84199c4c7184472
Alexander Simola Bergsten <alexandersb98@gmail.com>
2024-08-07 17:47:34 +0200

    Changed metadata in BepInPlugin attr

--- SCLauncher.cs
+++ SCLauncher.cs
@@ -2,7 +2,8 @@
 
 namespace SummonerCreator
 {
-	[BepInPlugin("teamgrad.summoner", "Summoner Creator", "1.0.7")]
+    // This mod is a fork of the original Summoner Creator mod by Team Grad.
+    [BepInPlugin(GUID: "asb.summoner", Name: "ASB Summoner Creator", Version: "1.0.0")]
 	public class SCLauncher : BaseUnityPlugin
 	{
 		public SCLauncher()
