125c34f61f132a1419ada3785c45bd078833f216
Alexander Simola Bergsten <alexandersb98@gmail.com>
2024-08-18 00:42:32 +0200

    Refactor SCMain ctor to use smaller methods

--- SCMain.cs
+++ SCMain.cs
@@ -13,19 +13,19 @@ namespace SummonerCreator
 {
     public class SCMain
     {
-        public List<UnitBlueprint> newUnits = new List<UnitBlueprint>();
-        public List<Faction> newFactions = new List<Faction>();
-        public List<TABSCampaignAsset> newCampaigns = new List<TABSCampaignAsset>();
-        public List<TABSCampaignLevelAsset> newCampaignLevels = new List<TABSCampaignLevelAsset>();
-        public List<VoiceBundle> newVoiceBundles = new List<VoiceBundle>();
-        public List<FactionIcon> newFactionIcons = new List<FactionIcon>();
-        public List<GameObject> newBases = new List<GameObject>();
-        public List<GameObject> newProps = new List<GameObject>();
-        public List<GameObject> newAbilities = new List<GameObject>();
-        public List<GameObject> newWeapons = new List<GameObject>();
-        public List<GameObject> newProjectiles = new List<GameObject>();
+        private IList<UnitBlueprint> newUnits = new List<UnitBlueprint>();
+        private IList<Faction> newFactions = new List<Faction>();
+        private IList<TABSCampaignAsset> newCampaigns = new List<TABSCampaignAsset>();
+        private IList<TABSCampaignLevelAsset> newCampaignLevels = new List<TABSCampaignLevelAsset>();
+        private IList<VoiceBundle> newVoiceBundles = new List<VoiceBundle>();
+        private IList<FactionIcon> newFactionIcons = new List<FactionIcon>();
+        private IList<GameObject> newBases = new List<GameObject>();
+        private IList<GameObject> newProps = new List<GameObject>();
+        private IList<GameObject> newAbilities = new List<GameObject>();
+        private IList<GameObject> newWeapons = new List<GameObject>();
+        private IList<GameObject> newProjectiles = new List<GameObject>();
 
-        public AssetBundle summon = AssetBundle.LoadFromMemory(Properties.Resources.summon);
+        private AssetBundle summon = AssetBundle.LoadFromMemory(Properties.Resources.summon);
 
         public SCMain()
         {
@@ -34,86 +34,132 @@ namespace SummonerCreator
                                             where !db.GetUnitBlueprint(unit.Entity.GUID).IsCustomUnit
                                             orderby db.GetUnitBlueprint(unit.Entity.GUID).Name
                                             select db.GetUnitBlueprint(unit.Entity.GUID)).ToList();
-            foreach (var fac in summon.LoadAllAssets<Faction>())
-            {
-                newFactions.Add(fac);
-            }
-            foreach (var unit in summon.LoadAllAssets<UnitBlueprint>())
-            {
-                newUnits.Add(unit);
-                foreach (var b in db.GetUnitBases().ToList()) { if (unit.UnitBase != null) { if (b.name == unit.UnitBase.name) { unit.UnitBase = b; } } }
-                foreach (var b in db.GetWeapons().ToList()) { if (unit.RightWeapon != null && b.name == unit.RightWeapon.name) unit.RightWeapon = b; if (unit.LeftWeapon != null && b.name == unit.LeftWeapon.name) unit.LeftWeapon = b; }
-            }
-            foreach (var objecting in summon.LoadAllAssets<GameObject>()) 
-            {
-                if (objecting != null) {
+            LoadFactions();
+            LoadUnitBlueprints(db);
+            LoadGameObjects();
 
-                    if (objecting.GetComponent<Unit>()) newBases.Add(objecting);
-                    else if (objecting.GetComponent<WeaponItem>()) {
-                        newWeapons.Add(objecting);
-                        int totalSubmeshes = 0;
-                        foreach (var rend in objecting.GetComponentsInChildren<MeshFilter>()) {
-                            if (rend.gameObject.activeSelf && rend.gameObject.activeInHierarchy && rend.mesh.subMeshCount > 0 && rend.GetComponent<MeshRenderer>() && rend.GetComponent<MeshRenderer>().enabled == true) {
-
-                                totalSubmeshes += rend.mesh.subMeshCount;
-                            }
-                        }
-                        foreach (var rend in objecting.GetComponentsInChildren<SkinnedMeshRenderer>()) {
-                            if (rend.gameObject.activeSelf && rend.sharedMesh.subMeshCount > 0 && rend.enabled) {
-
-                                totalSubmeshes += rend.sharedMesh.subMeshCount;
-                            }
-                        }
-                        if (totalSubmeshes != 0) {
-                            float average = 1f / totalSubmeshes;
-                            var averageList = new List<float>();
-                            for (int i = 0; i < totalSubmeshes; i++) { averageList.Add(average); }
-                            objecting.GetComponent<WeaponItem>().SubmeshArea = null;
-                            objecting.GetComponent<WeaponItem>().SubmeshArea = averageList.ToArray();
-                        }
-                    }
-                    else if (objecting.GetComponent<ProjectileEntity>()) newProjectiles.Add(objecting);
-                    else if (objecting.GetComponent<SpecialAbility>()) newAbilities.Add(objecting);
-                    else if (objecting.GetComponent<PropItem>()) {
-                        newProps.Add(objecting);
-                        int totalSubmeshes = 0;
-                        foreach (var rend in objecting.GetComponentsInChildren<MeshFilter>()) {
-                            if (rend.gameObject.activeSelf && rend.gameObject.activeInHierarchy && rend.mesh.subMeshCount > 0 && rend.GetComponent<MeshRenderer>() && rend.GetComponent<MeshRenderer>().enabled == true) {
-
-                                totalSubmeshes += rend.mesh.subMeshCount;
-                            }
-                        }
-                        foreach (var rend in objecting.GetComponentsInChildren<SkinnedMeshRenderer>()) {
-                            if (rend.gameObject.activeSelf && rend.sharedMesh.subMeshCount > 0 && rend.enabled) {
-
-                                totalSubmeshes += rend.sharedMesh.subMeshCount;
-                            }
-                        }
-                        if (totalSubmeshes != 0) {
-                            float average = 1f / totalSubmeshes;
-                            var averageList = new List<float>();
-                            for (int i = 0; i < totalSubmeshes; i++) { averageList.Add(average); }
-                            objecting.GetComponent<PropItem>().SubmeshArea = null;
-                            objecting.GetComponent<PropItem>().SubmeshArea = averageList.ToArray();
-                        }
-                    }
-                }
-            }
-            SummonerStats summonerStats = new SummonerStats
+            SummonerSingleton instance = SummonerSingleton.GetInstance();
+            instance.summonerFaction = summon.LoadAsset<Faction>("Summoners");
+            instance.summonerOriginal = summon.LoadAsset<UnitBlueprint>("Summoner");
+            instance.summonerStats = new SummonerStats // seems like this is where the default values of a summoner are set
             {
                 cooldown = 15f,
                 minionsPerSpawn = 5,
                 spawnables = new List<UnitBlueprint>(realList).ToArray()
-            };
-            SummonerSingleton shitstance = SummonerSingleton.GetInstance();
-            shitstance.summonerFaction = summon.LoadAsset<Faction>("Summoners");
-            shitstance.summonerOriginal = summon.LoadAsset<UnitBlueprint>("Summoner");
-            shitstance.summonerStats = summonerStats;
-            shitstance.ConfirmValues();
+            };;
+            instance.ConfirmValues();
 
             AddContentToDatabase();
         }
-        
+
+        private void LoadGameObjects()
+        {
+            foreach (var gameObject in summon.LoadAllAssets<GameObject>())
+            {
+                if (gameObject == null) continue;
+                
+                if (gameObject.GetComponent<Unit>())
+                {
+                    newBases.Add(gameObject);
+                }
+                else if (gameObject.GetComponent<WeaponItem>())
+                {
+                    newWeapons.Add(gameObject);
+                    HandleGameObject<WeaponItem>(gameObject);
+                }
+                else if (gameObject.GetComponent<ProjectileEntity>())
+                {
+                    newProjectiles.Add(gameObject);
+                }
+                else if (gameObject.GetComponent<SpecialAbility>())
+                {
+                    newAbilities.Add(gameObject);
+                }
+                else if (gameObject.GetComponent<PropItem>())
+                {
+                    newProps.Add(gameObject);
+                    HandleGameObject<PropItem>(gameObject);
+                }
+            }
+        }
+
+        // todo: rename method
+        private void HandleGameObject<T>(GameObject gameObject) where T : CharacterItem
+        {
+            int totalSubmeshes = 0;
+
+            foreach (var rend in gameObject.GetComponentsInChildren<MeshFilter>()) 
+            {
+                if (rend.gameObject.activeSelf 
+                    && rend.gameObject.activeInHierarchy 
+                    && rend.mesh.subMeshCount > 0 
+                    && rend.GetComponent<MeshRenderer>() != null
+                    && rend.GetComponent<MeshRenderer>().enabled == true) {
+
+                    totalSubmeshes += rend.mesh.subMeshCount;
+                }
+            }
+
+            foreach (var rend in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>()) 
+            {
+                if (rend.gameObject.activeSelf 
+                    && rend.sharedMesh.subMeshCount > 0 
+                    && rend.enabled) {
+
+                    totalSubmeshes += rend.sharedMesh.subMeshCount;
+                }
+            }
+
+            if (totalSubmeshes == 0) return;
+            
+            float average = 1f / totalSubmeshes;
+            var averageList = new List<float>();
+            for (int i = 0; i < totalSubmeshes; i++)
+            {
+                averageList.Add(average);
+            }
+
+            gameObject.GetComponent<T>().SubmeshArea = null; // todo: why is this row needed when we overwrite it on the the next row?
+            gameObject.GetComponent<T>().SubmeshArea = averageList.ToArray();
+        }
+
+        private void LoadUnitBlueprints(LandfallContentDatabase db)
+        {
+            foreach (var unit in summon.LoadAllAssets<UnitBlueprint>())
+            {
+                newUnits.Add(unit);
+
+                foreach (var unitBase in db.GetUnitBases().ToList()) // todo: why ToList()?
+                {
+                    if (unit.UnitBase != null && unitBase.name == unit.UnitBase.name)
+                    {
+                        unit.UnitBase = unitBase;
+                    }
+                }
+
+                foreach (var weapon in db.GetWeapons().ToList()) // todo: why ToList()?
+                {
+                    if (unit.RightWeapon != null && weapon.name == unit.RightWeapon.name)
+                    {
+                        unit.RightWeapon = weapon;
+                    }
+
+                    if (unit.LeftWeapon != null && weapon.name == unit.LeftWeapon.name)
+                    {
+                        unit.LeftWeapon = weapon;
+                    }
+                }
+            }
+        }
+
+        private void LoadFactions()
+        {
+            foreach (var fac in summon.LoadAllAssets<Faction>())
+            {
+                newFactions.Add(fac);
+            }
+        }
+
         public void AddContentToDatabase()
         {
             var contentAdder = new ModContentAdder(
@@ -132,6 +178,7 @@ namespace SummonerCreator
                 .AddWeaponsToDb(newWeapons)
                 .AddProjectilesToDb(newProjectiles);
 
+            // todo: document why
             ServiceLocator.GetService<CustomContentLoaderModIO>().QuickRefresh(WorkshopContentType.Unit, null);
         }
     }
